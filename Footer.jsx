88dd82fd155e15d86a90678fd0854e555bbed1c1
CyBoRgOmEgA <81644247+CyBoRgOmEgA@users.noreply.github.com>
2021-06-28 04:33:17 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Footer() {
+  return(
+    <footer>
+    <p>
+      Copyright by Shape AI @ {new Date().getFullYear()}
+    </p>   
+    </footer>  
+  );
+}
+
+export default Footer;
---
+++ Info.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Info(){
+  return(
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>a basic web development React.js bootcamp</p>
+    </div>
+  );
+}
+
+export default Info;
